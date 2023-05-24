c6a0a0c121c3a5ef9823874520351e1cc33db606
Sofiia Matveeva <sofiia.matve.va@gmail.com>
2023-05-24 19:01:32 +0200

    env: set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'main.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: { minimize: true },
+					},
+				],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader',
+					{
+						loader: 'sass-resources-loader',
+						options: {
+							resources: [
+								'./src/styles/vars.scss',
+							],
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: './index.html',
+		}),
+	],
+	devServer: {
+		compress: true,
+		port: 4200,
+	},
+};
