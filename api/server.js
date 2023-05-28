3de241d1fcd6a7e6723ecbf789be90831da6fc74
Sofiia Matveeva <sofiia.matve.va@gmail.com>
2023-05-28 17:06:50 +0200

    server: set up server paths for assets

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const jsonServer = require('json-server');
 const cors = require('cors');
 const path = require('path');
+const express = require('express');
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'database/db.json'));
 const middlewares = jsonServer.defaults();
 
+server.use('/static', express.static(path.join(__dirname, 'public')))
 server.use(cors());
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
