051ca347fac893a541e0d72102be30f354955aba
Sofiia Matveeva <sofiia.matve.va@gmail.com>
2023-06-06 20:22:25 +0200

    feat: add artist widget

---
+++ src/collections/artists.collection.js
@@ -0,0 +1,28 @@
+import Artist from "../models/Artist";
+
+export default class ArtistsCollection {
+	constructor(ApiService) {
+		this.apiService = ApiService;
+		this.artistsPromise = null;
+		this.artistsLibrary = null;
+	}
+
+	async getAllArtists() {
+		try {
+			if (!this.artistsPromise) {
+				this.artistsPromise = this.apiService.getAllArtists();
+			}
+
+			return this.artistsPromise;
+		} catch (error) {
+			console.error(error);
+			throw error;
+		}
+	}
+
+	saveArtistsLibrary(artists) {
+		const library = artists.map((artist) => new Artist(this.apiService, artist));
+
+		this.artistsLibrary = library;
+	}
+};
--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,4 +1,5 @@
 export const BASE_URL = 'http://localhost:3000';
 export const PROD_URL = 'http://localhost:3000';
 export const GET_ALL_SONGS = `${BASE_URL}/songs`;
+export const GET_ALL_ARTISTS = `${BASE_URL}/artists`;
 export const IMAGES_URL = `${BASE_URL}/static`;
