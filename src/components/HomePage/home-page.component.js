53cfb46527af0135a4582dfa2d595ad85a88868e
Sofiia Matveeva <sofiia.matve.va@gmail.com>
2023-06-08 20:13:59 +0200

    feat: add genres on home page

--- src/components/HomePage/home-page.component.js
+++ src/components/HomePage/home-page.component.js
@@ -3,10 +3,12 @@ import HomePageTemplate from './home-page.component.html';
 import './home-page.component.scss';
 import PlaylistComponent from '../Playlist/playlist.component';
 import ArtistListComponent from '../ArtistList/artist-list.component';
+import GenresListComponent from '../GenresList/genres-list.component';
 
 const HomePageComponent = convertToHtml(HomePageTemplate);
 
 HomePageComponent.appendChild(PlaylistComponent);
 HomePageComponent.appendChild(ArtistListComponent);
+HomePageComponent.appendChild(GenresListComponent);
 
 export default HomePageComponent;
