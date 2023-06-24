6b514665565d8fe105172d86ff02dffab97ad357
Sofiia Matveeva <sofiia.matve.va@gmail.com>
2023-06-24 15:19:21 +0200

    feat: add styles for genres slider

--- src/components/GenresList/genres-list.component.js
+++ src/components/GenresList/genres-list.component.js
@@ -3,18 +3,19 @@ import ApiService from '../../services/api.service';
 import convertToHtml from '../../utils/htmlElement';
 import GenreWidgetComponent from '../GenreWidget/genre-widget.component';
 import GenresListTemplate from './genres-list.component.html';
+import './genres-list.component.scss';
 
 const GenresListComponent = convertToHtml(GenresListTemplate);
 const genresCollection = new GenresCollection(ApiService);
 
 genresCollection.getAllGenres().then((genresLibrary) => {
+	const GENRES_LIST_ELEMENT = document.getElementById('genres-list');
 	genresCollection.saveGenresLibrary(genresLibrary);
 
 	genresCollection.genresLibrary.forEach((genre) => {
 		const GENRE_ELEMENT = GenreWidgetComponent(genre);
 
-		console.log(GenresListComponent);
-		GenresListComponent.appendChild(GENRE_ELEMENT);
+		GENRES_LIST_ELEMENT.appendChild(GENRE_ELEMENT);
 	});
 });
 
